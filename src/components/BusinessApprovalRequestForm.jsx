020e2d43dfe051a8ab23c8f1b8291161c2c8c199
Lucky <25218389+luckyvs1@users.noreply.github.com>
2018-07-19 17:32:39 -0700

    Update frontend of business signup to resuse styles from user signup

--- src/components/BusinessApprovalRequestForm.jsx
+++ src/components/BusinessApprovalRequestForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel, Col, Form } from 'react-bootstrap';
+import { Button, FormGroup, FormControl, ControlLabel, Form } from 'react-bootstrap';
+import '../styles/BusinessApprovalRequestForm.css'
 
 class BusinessApprovalRequestForm extends Component {
 
@@ -13,79 +14,57 @@ class BusinessApprovalRequestForm extends Component {
       <div className="BusinessApprovalRequest">
         <Form horizontal>
           <FormGroup controlId="formHorizontalName">
-            <Col componentClass={ControlLabel} sm={2}>
-              Business Name
-            </Col>
-            <Col sm={10}>
-              <FormControl
-                type="text"
-                placeholder="Business Name"
-                value={this.state.email}
-                onChange={e=>this.setState({name: e.target.value})}
-              />
-            </Col>
+            <ControlLabel> Business Name </ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Business Name"
+              value={this.state.email}
+              onChange={e=>this.setState({name: e.target.value})}
+            />
           </FormGroup>
 
           <FormGroup controlId="formHorizontalDescription">
-            <Col componentClass={ControlLabel} sm={2}>
-              Business Description
-            </Col>
-            <Col sm={10}>
-              <FormControl
-                type="text"
-                placeholder="Business Description"
-                value={this.state.description}
-                onChange={e=>this.setState({description: e.target.value})}
-              />
-            </Col>
+            <ControlLabel> Business Description </ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Business Description"
+              value={this.state.description}
+              onChange={e=>this.setState({description: e.target.value})}
+            />
           </FormGroup>
 
           <FormGroup controlId="formHorizontalWebsite">
-            <Col componentClass={ControlLabel} sm={2}>
-              Phone Number
-            </Col>
-            <Col sm={10}>
-              <FormControl
-                type="text"
-                placeholder="Phone Number"
-                value={this.state.website}
-                onChange={e=>this.setState({phonenumber: e.target.value})}
-              />
-            </Col>
+            <ControlLabel> Phone Number </ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Phone Number"
+              value={this.state.website}
+              onChange={e=>this.setState({phonenumber: e.target.value})}
+            />
           </FormGroup>
 
           <FormGroup controlId="formHorizontalWebsite">
-            <Col componentClass={ControlLabel} sm={2}>
-              Address
-            </Col>
-            <Col sm={10}>
-              <FormControl
-                type="text"
-                placeholder="Address"
-                value={this.state.website}
-                onChange={e=>this.setState({address: e.target.value})}
-              />
-            </Col>
+            <ControlLabel> Address </ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Address"
+              value={this.state.website}
+              onChange={e=>this.setState({address: e.target.value})}
+            />
           </FormGroup>
 
           <FormGroup controlId="formHorizontalWebsite">
-            <Col componentClass={ControlLabel} sm={2}>
-              Website
-            </Col>
-            <Col sm={10}>
-              <FormControl
-                type="text"
-                placeholder="Business Website"
-                value={this.state.website}
-                onChange={e=>this.setState({website: e.target.value})}
-              />
-            </Col>
+            <ControlLabel> Website </ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Business Website"
+              value={this.state.website}
+              onChange={e=>this.setState({website: e.target.value})}
+            />
           </FormGroup>
 
           <FormGroup>
-            <Col smOffset={2} sm={10}>
-              <Button type="submit">Request Approval</Button>
-            </Col>
+            <Button type="submit">Request Approval</Button>
           </FormGroup>
         </Form>;
       </div>
