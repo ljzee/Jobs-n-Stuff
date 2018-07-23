7a038572af2c39623726fbcb62552307b05f96d0
Lucky <25218389+luckyvs1@users.noreply.github.com>
2018-07-22 21:18:32 -0700

    Render business approval form if the business user is unapproved

--- src/components/BusinessPageForm.jsx
+++ src/components/BusinessPageForm.jsx
@@ -1,29 +1,84 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { AUTH_TOKEN } from '../constants';
+import { AUTH_TOKEN, USER_TOKEN } from '../constants';
+import { graphql, compose } from 'react-apollo';
+import { withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withRouter } from 'react-router-dom';
+
 import BusinessApprovalRequestForm from './BusinessApprovalRequestForm';
+import Loading from './Loading';
+
+const userToken = localStorage.getItem(USER_TOKEN)
+const authToken = localStorage.getItem(AUTH_TOKEN)
 
 class BusinessPageForm extends Component {
-  render() {
-    const authToken = localStorage.getItem(AUTH_TOKEN)
 
-    if(!authToken) {
-      return (
-        <Redirect to='login'/>
-      )
+  render() {
+
+    if (this.props.userQuery.loading) {
+      return <Loading />;
     }
 
+    if (this.props.userQuery.error) {
+      return <Redirect to='/login'/>;
+    }
+
+    if(authToken) {
+      return (
+        <div className="Profile">
+          {this.props.userQuery.user.activated === false
+          ? this.businessApprovalRequest()
+          : this.businessDetails()}
+        </div>
+      );
+    } else {
+      return (
+        <Redirect to='login'/>
+      );
+    }
+  }
+
+  businessApprovalRequest() {
     return (
-      <div className="BusinessPageForm">
-        <h1>This is a business profile</h1>
+      <div className="BusinessApprovalRequest">
+        <h1>This is an unapproved business profile</h1>
         {/* TODO: If the business user is not approved render the business approval form */}
         <BusinessApprovalRequestForm />
-        {/* Else render the profile form reusuable part -- shared with user*/}
+      </div>
+    );
+  }
 
-        {/* Also render the profile form nonreusable part -- unique to business user*/}
+  businessDetails(){
+    return (
+      <div className="BusinessDetails">
+        <h1>This is an approved business profile potatoe</h1>
+        {/* TODO: Render the business details form*/}
       </div>
     );
   }
 }
 
-export default BusinessPageForm;
+const USER_QUERY = gql`
+  query UserQuery($where: UserWhereUniqueInput!) {
+    user(where: $where) {
+      username
+      activated
+    }
+  }
+`
+
+export default compose(
+  graphql(USER_QUERY, {
+    name: 'userQuery',
+    options: props => ({
+      variables: {
+          where: {
+            id:JSON.parse(userToken).id
+          }
+        },
+    }),
+  }),
+  withRouter,
+  withApollo
+)(BusinessPageForm)
